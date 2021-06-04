561e902dff0a5ad0f717d2be01d08757c3cdf475
Dounia <dounia913@gmail.com>
2021-06-04 09:49:51 +0200

    test commit

--- src/application/Main.java
+++ src/application/Main.java
@@ -25,9 +25,11 @@ public class Main extends Application {
 			scene.getStylesheets().add(getClass().getResource("application.css").toExternalForm());
 			primaryStage.setScene(scene);
 			primaryStage.show();
+			
 		} catch(Exception e) {
 			e.printStackTrace();
 		}
+		System.out.println("hello world");
 	}
 	
 	public static void main(String[] args) {
