8695b05e8df06f7da311dac821dd517a7dfa0bcd
Dounia <dounia913@gmail.com>
2021-06-04 11:52:11 +0200

    add jsonreader

---
+++ src/model/ApiResquester.java
@@ -0,0 +1,42 @@
+package model;
+
+import org.json.JSONObject;
+
+import java.net.URI;
+import java.net.http.HttpClient;
+import java.net.http.HttpRequest;
+import java.net.http.HttpResponse;
+import java.net.http.HttpClient.Redirect;
+import java.net.http.HttpClient.Version;
+import java.net.http.HttpResponse.BodyHandlers;
+import java.time.Duration;
+import java.util.concurrent.TimeUnit;
+
+
+public class ApiResquester {
+	public static JSONObject readJsonFromUrl(String url) {
+    	String json = "";
+    	HttpClient client = HttpClient.newBuilder()
+    			.version(Version.HTTP_1_1)
+    			.followRedirects(Redirect.NORMAL)
+    			.connectTimeout(Duration.ofSeconds(20))
+    			.build();
+    	HttpRequest request = HttpRequest.newBuilder()
+    			.uri(URI.create(url))
+    			.timeout(Duration.ofMinutes(2))
+    			.header("Content-Type", "application/json")
+    		.GET()
+    		.build();
+    	try {
+    		json = client.sendAsync(request, BodyHandlers.ofString())
+    				.thenApply(HttpResponse::body).get(10,TimeUnit.SECONDS);
+    	}catch(Exception e) {
+    		e.printStackTrace();
+    	}
+    	return new JSONObject(json);
+    }
+	
+	public static void main(String[] args) {
+		readJsonFromUrl("https://api.obis.org/v3/occurrence/grid/3?scientificname=Delphinidae");    
+	}
+}
--- src/model/JsonReader.java
+++ src/model/JsonReader.java
@@ -1,23 +1,8 @@
 package model;
 
-import java.io.BufferedReader;
 
-import java.io.FileReader;
 import java.io.IOException;
 import java.io.Reader;
-import java.net.URI;
-import java.time.Duration;
-import java.util.concurrent.TimeUnit;
-
-import org.json.JSONArray;
-import org.json.JSONObject;
-
-import java.net.http.HttpClient;
-import java.net.http.HttpClient.Redirect;
-import java.net.http.HttpClient.Version;
-import java.net.http.HttpRequest;
-import java.net.http.HttpResponse;
-import java.net.http.HttpResponse.BodyHandlers;
 
 public class JsonReader {
 
@@ -30,27 +15,7 @@ public class JsonReader {
     	return sb.toString();
     }
     
-    public static JSONObject readJsonFromUrl(String url) {
-    	String json = "";
-    	HttpClient client = HttpClient.newBuilder()
-    			.version(Version.HTTP_1_1)
-    			.followRedirects(Redirect.NORMAL)
-    			.connectTimeout(Duration.ofSeconds(20))
-    			.build();
-    	HttpRequest request = HttpRequest.newBuilder()
-    			.uri(URI.create(url))
-    			.timeout(Duration.ofMinutes(2))
-    			.header("Content-Type", "application/json")
-    		.GET()
-    		.build();
-    	try {
-    		json = client.sendAsync(request, BodyHandlers.ofString())
-    				.thenApply(HttpResponse::body).get(10,TimeUnit.SECONDS);
-    	}catch(Exception e) {
-    		e.printStackTrace();
-    	}
-    	return new JSONObject(json);
-    }
+    
     
 	public static void main(String[] args) {
 		/*
@@ -71,6 +36,5 @@ public class JsonReader {
 	    	e.printStackTrace();
 	    }
 	    */
-		readJsonFromUrl("https://api.obis.org/v3/occurrence");    
 	}
 }
